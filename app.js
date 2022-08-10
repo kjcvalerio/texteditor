f240cab2128107d95f46360740af1e24a60affdd
Kelvin Valério <kelvincuango@gmail.com>
2022-08-10 11:28:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function italic() {document.getElementById('textarea').style.fontStyle="italic";}
+function capitalize() {document.getElementById('textarea').style.textTransform="uppercase";}
+function align_center() {document.getElementById('textarea').style.textAlign="center"}
+function align_right() {document.getElementById('textarea').style.textAlign="right"}
+function align_left() {document.getElementById('textarea').style.textAlign="left"}
+function underline() {document.getElementById('textarea').style.textDecoration="underline"}
+function strikethrough() {document.getElementById('textarea').style.textDecoration="line-through"}
+function bold() {document.getElementById('textarea').style.fontWeight="bold"}
+
+function timesnewroman(){document.getElementById('timesnewroman').style.fontFamily="'Times New Roman', Times, serif"}
+function verdana(){document.getElementById('verdana').style.fontFamily="Verdana, Geneva, Tahoma, sans-serif"}
+function arial(){document.getElementById('arial').style.fontFamily="Arial, Helvetica, sans-serif"}
